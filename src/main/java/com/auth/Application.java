db19ea86381d7a5f2bfc46a963b41b070ec17a98
Mad-Deuce <zolotukhinpv@gmail.com>
2022-06-16 20:43:24 +0300

    add layout dialect

--- src/main/java/com/auth/Application.java
+++ src/main/java/com/auth/Application.java
@@ -1,11 +1,30 @@
 package com.auth;
 
+import nz.net.ultraq.thymeleaf.layoutdialect.LayoutDialect;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.thymeleaf.spring5.SpringTemplateEngine;
 
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
+
+
+    @Bean
+    public LayoutDialect layoutDialect() {
+        return new LayoutDialect();
+    }
+
+//    @Bean
+//    public SpringTemplateEngine templateEngine() {
+//        SpringTemplateEngine templateEngine = new SpringTemplateEngine();
+//
+//        templateEngine.addDialect(new LayoutDialect());
+//        return templateEngine;
+//    }
+
+
 }
--- src/main/java/com/auth/config/WebSecurityConfig.java
+++ src/main/java/com/auth/config/WebSecurityConfig.java
@@ -33,7 +33,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                     .antMatchers("/admin/**").hasRole("ADMIN")
                     .antMatchers("/news").hasRole("USER")
                     //Доступ разрешен всем пользователей
-                    .antMatchers("/", "/resources/**", "/css/**").permitAll()
+                    .antMatchers("/", "/resources/**", "/css/**", "/fragments/**", "/layouts/**").permitAll()
                 //Все остальные страницы требуют аутентификации
                 .anyRequest().authenticated()
                 .and()
