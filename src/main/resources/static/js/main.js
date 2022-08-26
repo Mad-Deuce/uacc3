55b67a847fd223ae6fe8b3773e6c1c2b2773d340
admin <zolotukhinpv@gmail.com>
2022-08-26 19:12:16 +0300

    add pagination control element

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -14,7 +14,7 @@ let requestDTO = {
     sort: "",
     //pagination
     size: "30",
-    page: "1"
+    page: ""
 };
 
 
@@ -86,7 +86,7 @@ function outputHTML() {
 }
 
 //update contentData
-function updateHTML(){
+function updateHTML() {
     let regExpForSub = /<!--start_sub_template-->.*<!--end_sub_template-->/gms;
     let subTemplateText = templateText.match(regExpForSub);
     let subFn = _.template(subTemplateText[0]);
@@ -121,11 +121,53 @@ contentElement.on("input", "#filter_byGrid", function (param) {
     getContent(uri);
 })
 
-contentElement.on("change", "#item_on_page_input", function (param) {
-
-    requestDTO.size = $("#item_on_page_input").val();
+contentElement.on("change", "#range_item_on_page", function (param) {
+    requestDTO.page="0";
+    // contentData.number=0;
+    requestDTO.size = $("#range_item_on_page").val();
     isUpdate = true;
-    document.getElementById("item_on_page_output").textContent = "Items on page " + $("#item_on_page_input").val();
     getContent(uri);
 
+    document.getElementById("item_on_page_output").textContent = "Items on page " + $("#range_item_on_page").val();
+    document.getElementById("input_find_page").value = (contentData.number + 1) + " out of " + contentData.totalPages;
+})
+
+contentElement.on("keypress", "#input_find_page", function (param) {
+    if (param.which === 13) {           //13 - это код клавиши "Enter"
+        if (!isNaN($("#input_find_page").val())) {
+            requestDTO.page = $("#input_find_page").val() - 1;
+            isUpdate = true;
+            getContent(uri);
+        }
+        document.getElementById("input_find_page").blur();
+        document.getElementById("input_find_page").value = (contentData.number + 1) + " out of " + contentData.totalPages;
+    }
+})
+
+contentElement.on("click", "#btn_first_page", function () {
+    requestDTO.page = "0";
+    isUpdate = true;
+    getContent(uri);
+    document.getElementById("input_find_page").value = (contentData.number + 1) + " out of " + contentData.totalPages;
+})
+contentElement.on("click", "#btn_last_page", function () {
+    requestDTO.page = contentData.totalPages - 1;
+    isUpdate = true;
+    getContent(uri);
+    document.getElementById("input_find_page").value = (contentData.number + 1) + " out of " + contentData.totalPages;
+})
+
+contentElement.on("click", "#btn_prev_page", function () {
+    // requestDTO.page = "0";
+    requestDTO.page = (contentData.number <= 0 ? 0 : contentData.number - 1);
+    isUpdate = true;
+    getContent(uri);
+    document.getElementById("input_find_page").value = (contentData.number + 1) + " out of " + contentData.totalPages;
+})
+contentElement.on("click", "#btn_next_page", function () {
+    // requestDTO.page = "0";
+    requestDTO.page = (contentData.number >= contentData.totalPages - 1 ? contentData.totalPages - 1 : contentData.number + 1);
+    isUpdate = true;
+    getContent(uri);
+    document.getElementById("input_find_page").value = (contentData.number + 1) + " out of " + contentData.totalPages;
 })
