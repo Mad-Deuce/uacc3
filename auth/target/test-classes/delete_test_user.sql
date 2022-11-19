0a7445832a1227c79fcb57027e5bd47af5e39a5f
admin <zolotukhinpv@gmail.com>
2022-11-19 19:32:04 +0200

    remove target files

--- auth/target/test-classes/delete_test_user.sql
+++
@@ -1,8 +0,0 @@
-DELETE
-FROM public.users_roles
-WHERE user_id = (SELECT id
-                 FROM public.users
-                 WHERE name = 'test_username');
-DELETE
-FROM public.users
-WHERE name = 'test_username';
