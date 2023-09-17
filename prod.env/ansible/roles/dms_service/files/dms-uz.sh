359cabdf9248a14e79550c0e2c46de30028b433e
admin <zolotukhinpv@gmail.com>
2023-09-17 18:51:10 +0300

    ref prod.env Vagrantfile

---
+++ prod.env/ansible/roles/dms_service/files/dms-uz.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+#sudo systemctl daemon-reload
+#sudo /usr/bin/java -version
+sudo /usr/bin/java -jar dms-uz-rtubase-1.0-SNAPSHOT.jar
+#echo ----------------------------------------------------
+#sudo systemctl daemon-reload
+#sudo systemctl start dms-uz.service
